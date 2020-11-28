bd9bb846285448f9af85a7bf3a2a1faad135f5f7
varma_creator <mandativarmayadav@gmail.com>
2020-11-28 14:48:35 +0530

    added comments

--- OPV/src/main/java/com/psa/opv/newvehicle/OpvApplication.java
+++ OPV/src/main/java/com/psa/opv/newvehicle/OpvApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * This Class represents the Springboot App
+ * @author Varma
+ *
+ */
 @EnableJpaRepositories(basePackages = "com.psa.opv.newvehicle.repository")
 @SpringBootApplication
 public class OpvApplication {
