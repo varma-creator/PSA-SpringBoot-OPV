e0c963ad6ce4549dcbc8c8e9001d9f0b892a6ee6
varma_creator <mandativarmayadav@gmail.com>
2020-12-17 17:45:39 +0530

    API gateway-Microservices

---
+++ OPV-Gateway/src/test/java/com/psa/opv/newvehicle/gateway/OpvGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psa.opv.newvehicle.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpvGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
