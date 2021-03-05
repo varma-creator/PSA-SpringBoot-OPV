0f4a982ac1f9bf7c6455929c22a2ef4ead4027f7
varma_creator <mandativarmayadav@gmail.com>
2021-03-05 19:17:29 +0530

    Commit  local changes

--- OPV/src/main/java/com/psa/opv/newvehicle/OpvApplication.java
+++ OPV/src/main/java/com/psa/opv/newvehicle/OpvApplication.java
@@ -14,7 +14,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableCaching
 @EnableEurekaClient
 public class OpvApplication {
-
+//local
 	public static void main(String[] args) {
 		//conflictremote
 		SpringApplication.run(OpvApplication.class, args);
